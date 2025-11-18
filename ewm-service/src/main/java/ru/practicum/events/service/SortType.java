93cd71ea5cf9c63a30a506aa9532365bc7e026e8
Yulia Kudlai <iu.5hi@yandex.ru>
2025-11-18 16:49:58 +0300

    часть 2

---
+++ ewm-service/src/main/java/ru/practicum/events/service/SortType.java
@@ -0,0 +1,15 @@
+package ru.practicum.events.service;
+
+public  enum SortType {
+    EVENT_DATE,
+    VIEWS;
+
+    public static SortType from(String str) {
+        if (str == null) return EVENT_DATE;
+        return switch (str.toUpperCase()) {
+            case "EVENT_DATE" -> EVENT_DATE;
+            case "VIEWS" -> VIEWS;
+            default -> throw new IllegalArgumentException("Unknown sort type: " + str);
+        };
+    }
+}
